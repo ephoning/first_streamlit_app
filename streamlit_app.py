80aa2ffe716c24e631c80a4811ebf70c3aecd066
Edgar Honing <ehoning@vmware.com>
2023-10-03 16:09:31 -0600

    add fruit selection error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,13 +25,16 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input("Choose fruit", "Kiwi")
-streamlit.write("The user entered", fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+    fruit_choice = streamlit.text_input("Choose fruit")
+    if not fruit_choice:
+        streamlit.error("Please select a fruite to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 # TEMP stop
 streamlit.stop()
