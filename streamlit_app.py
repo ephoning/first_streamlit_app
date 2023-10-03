8984c04fec8a78b1237c38a551aaf695822050bb
Edgar Honing <ehoning@vmware.com>
2023-10-03 12:22:08 -0600

    temp df to txt display due to css laoding error

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,9 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # show DF
-streamlit.dataframe(fruits_to_show)
+streamlit.dataftextrame(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+streamlit.text(fruityvice_normalized)
