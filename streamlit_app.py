1b0223e3b3ed38003e9556bc2eeb902f90c7193f
Edgar Honing <ehoning@vmware.com>
2023-10-03 12:27:32 -0600

    add fruit selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.text(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+fruit_choice = streamlit.text_input("Choose fruit", "Kiwi")
+streamlit.write("The user entered", fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.text(fruityvice_normalized)
