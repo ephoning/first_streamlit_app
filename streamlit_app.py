387afbc1d13db066ca32d8c609e965505817e5f7
Edgar Honing <ehoning@vmware.com>
2023-10-03 14:59:58 -0600

    show all fruit as df

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,9 @@ my_cur = my_cnx.cursor()
 #streamlit.text("Hello from Snowflake:")
 
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-#streamlit.text("The fruit load list contains:")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
 
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_rows)
+
 
